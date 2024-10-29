1a3150fb5cf170d33c02fafdfde13ef19b9fe934
elexfreeman <elextraza@gmail.com>
2024-10-29 14:49:21 +0100

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
-use actix_files::Files;
-use actix_files::NamedFile;
-use actix_web::HttpRequest;
-use std::path::PathBuf;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-async fn index(req: HttpRequest) -> actix_web::Result<NamedFile> {
-    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
-    Ok(NamedFile::open(path)?)
+#[get("/")]
+async fn hello() -> impl Responder {
+        HttpResponse::Ok().body("{}")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+        println!("{}",req_body);
+        HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+        HttpResponse::Ok().body("Hey there!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    use actix_web::{web, App, HttpServer};
-    let app_port = 8088;
-
-    println!("Server start at 8088");
-    HttpServer::new(|| App::new().service(
-        Files::new("/", "./static")
-                .show_files_listing()
-                .index_file("index.html")
-                .use_last_modified(true)
-        .prefer_utf8(true)))
-        .bind(format!("[::1]:{}", app_port))?
+        HttpServer::new(|| {
+                    App::new()
+                    .service(hello)
+                    .service(echo)
+                    .route("/hey", web::get().to(manual_hello))
+        })
+        .bind(("0.0.0.0", 8088))?
         .run()
         .await
 }
