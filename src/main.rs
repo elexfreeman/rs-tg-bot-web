2835e015a5e7ac66728ab2542fe978ababb6fed1
apolozov <aleks.polozov@x5.ru>
2024-10-29 17:00:32 +0300

    add cors

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,40 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_cors::Cors;
+use actix_web::{get, http, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-        HttpResponse::Ok().body("{}")
+    HttpResponse::Ok().body("{}")
 }
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
-        println!("{}",req_body);
-        HttpResponse::Ok().body(req_body)
+    println!("{}", req_body);
+    HttpResponse::Ok().body(req_body)
 }
 
 async fn manual_hello() -> impl Responder {
-        HttpResponse::Ok().body("Hey there!")
+    HttpResponse::Ok().body("Hey there!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-        HttpServer::new(|| {
-                    App::new()
-                    .service(hello)
-                    .service(echo)
-                    .route("/hey", web::get().to(manual_hello))
-        })
-        .bind(("0.0.0.0", 8088))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin("*")
+            .allowed_methods(vec!["GET", "POST", "DELETE", "PUT"])
+            .allowed_headers(vec![
+                http::header::AUTHORIZATION,
+                http::header::ACCEPT,
+                http::header::CONTENT_TYPE,
+            ])
+            .max_age(3600);
+        App::new()
+            .wrap(cors)
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("0.0.0.0", 8088))?
+    .run()
+    .await
 }
